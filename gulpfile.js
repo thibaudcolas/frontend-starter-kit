f1fb3f9d73ecf50ade68a61d8c560ab850307124
Thibaud Colas <thibaudcolas@gmail.com>
2015-06-12 12:02:17 +1200

    Switch to browser-sync for live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 var gulp = require("gulp");
 
+require("./gulp/watch");
 require("./gulp/js");
 require("./gulp/css");
 require("./gulp/svg");
-require("./gulp/watch");
-
 
 gulp.task('build', ['js', 'css', 'svg'], function(done) {
     done();
 });
 
 gulp.task('default', ['build']);
-
