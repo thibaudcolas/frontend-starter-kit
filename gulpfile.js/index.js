6aed6ac24ca1c2983eb26ada55227fa43a06d29d
Thibaud Colas <thibaudcolas@gmail.com>
2016-02-05 17:47:48 +0200

    Add back build task

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,3 +2,9 @@ require('./tasks/watch');
 require('./tasks/css');
 require('./tasks/js');
 require('./tasks/svg');
+
+var gulp = require('gulp');
+
+gulp.task('build', ['js', 'css', 'svg']);
+
+gulp.task('default', ['build']);
