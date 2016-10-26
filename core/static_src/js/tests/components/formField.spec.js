b8ec4424a9dc60f5e40db2b48bf8f078c3b1495c
Vincent Audebert <vincent.audebert@yahoo.fr>
2016-10-26 13:15:02 +1300

    Adding 'minimum test' which uses only required properties.

--- core/static_src/js/tests/components/formField.spec.js
+++ core/static_src/js/tests/components/formField.spec.js
@@ -3,6 +3,13 @@ import FormField from '../../components/FormField';
 import React from 'react';
 
 describe('FormField', () => {
+    it('minimum', () => {
+        const tree = renderer.create(
+            <FormField id="field1" />
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
     it('renders correctly', () => {
         const tree = renderer.create(
             <FormField id="field1" name="field1" value="Hello world!" />
