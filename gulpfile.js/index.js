9286a679bb24a0817bc38ed93c973b6d56e0ce95
Thibaud Colas <thibaudcolas@gmail.com>
2016-12-07 11:57:07 +1300

    Clean up gulp tasks code

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,9 +1,9 @@
+const gulp = require('gulp');
+
 require('./tasks/watch');
 require('./tasks/css');
 require('./tasks/js');
 require('./tasks/svg');
-
-var gulp = require('gulp');
 
 gulp.task('build', ['js', 'css', 'svg']);
 
