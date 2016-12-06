2f5520b01278a1defae2becfea8a3030699235d7
Thibaud Colas <thibaudcolas@gmail.com>
2016-12-07 11:53:25 +1300

    Remove unused svg:inline gulp task

--- gulpfile.js/tasks/svg.js
+++ gulpfile.js/tasks/svg.js
@@ -16,21 +16,3 @@ gulp.task('svg', function() {
         .pipe(size({ title: 'SVG', gzip: config.prod }))
         .pipe(gulp.dest(config.paths.images));
 });
-
-gulp.task('svg:inline', function() {
-    var svgs = gulp
-        .src(path.join(config.paths.svg, '**', '*.svg'))
-        .pipe(rename({ prefix: 'i-' }))
-        .pipe(svgmin())
-        .pipe(svgstore({ inlineSvg: true }))
-        .pipe(size({ title: 'SVG', gzip: config.prod }));
-
-    function fileContents(filePath, file) {
-        return file.contents.toString();
-    }
-
-    return gulp
-        .src(path.join(config.paths.views, 'index.html'))
-        .pipe(inject(svgs, { transform: fileContents }))
-        .pipe(gulp.dest(config.paths.views));
-});
